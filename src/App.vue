c7c24a6cb86333babcf81057ec591026732f50f0
Rob Leroy <robleroy@msn.com>
2020-12-18 11:45:02 -0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,53 @@
+<script>
+  export default {
+    name: 'App',
+    data() {
+      return {
+        title: "Vue 3!"
+      }
+    }
+  }
+</script>
+
+<template>
+  <div id="appgrid" v-cloak>
+    <header>
+      <h4>{{title}}</h4>
+
+      <!-- <div id="nav">
+        <router-link to="/">Home</router-link> |
+        <router-link to="/about">About</router-link>
+      </div> -->
+    </header>
+
+    <main>
+      <!-- <router-view v-slot="{ Component }">
+        <transition name='fade' mode='out-in'>
+          <component :is="Component"/>
+        </transition>
+      </router-view> -->
+    </main>
+  </div> 
+</template>
+
+<style lang="scss">
+  @import './scss/baseline';
+
+  #appgrid {
+    height: 100vh;
+    display: grid;
+    grid-template-rows: auto 1fr;
+  }
+
+  header, main {
+    padding: 1rem 2rem;
+  }
+
+  header {
+    display: grid;
+    grid-auto-flow: column;
+    justify-content: space-between;
+
+    @include shadow(3);
+  }
+</style>
